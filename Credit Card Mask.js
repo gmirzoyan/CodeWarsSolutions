602fdb046230b7ab34e840a214cb9eaadddc8410
Gayane Mirzoyan <gayusha@gmail.com>
2020-08-20 01:16:31 -0700

    Add CodeWars Solution 'Credit Card Mask'

---
+++ Credit Card Mask.js
@@ -0,0 +1,13 @@
+//https://www.codewars.com/kata/5412509bd436bd33920011bc/train/javascript
+
+function maskify(cc) {
+    let str = cc.slice(-4);
+    let str1 = '';
+    for (let i = 0; i < cc.length - 4; i++){
+        cc[i] === '#';
+        str1 = str1 + '#';
+    }
+    return str1 + str;
+}
+
+console.log(maskify('4556364607935616'))
