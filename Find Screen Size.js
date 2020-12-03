30b64f4f056108cc4b28faf2cb6cc5e80481696a
Gayane Mirzoyan <gayusha@gmail.com>
2020-12-03 02:10:26 -0800

    Add CodeWars Solution 'Find Screen Size'

---
+++ Find Screen Size.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/5bbd279c8f8bbd5ee500000f/train/javascript
+
+function findScreenHeight(width, ratio) {
+    let res = ratio.split(':');
+    let result = width * +res[1] / +res[0];
+    return `${width}x${result}`;
+}
+
+console.log(findScreenHeight((1024,"4:3"));
