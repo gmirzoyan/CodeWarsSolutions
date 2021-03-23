1fce317bcb2a3a4354f537c92f8993450d1aab1a
Gayane Mirzoyan <gayusha@gmail.com>
2021-03-22 23:32:09 -0700

    Add CodeWars Solution 'isEven? - Bitwise Series'

---
+++ isEven? - Bitwise Series.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/592a33e549fe9840a8000ba1/train/javascript
+
+let isEven = function (n) {
+    return Number.isInteger(n / 2) ? true : false;
+}
+
+console.log(isEven(25));
