7308d91e3d04d5130ef6c273c9d4c4d6766f7fc0
Gayane Mirzoyan <gayusha@gmail.com>
2021-03-25 22:47:23 -0700

    Add CodeWars Solution 'Thinkful - String Drills: Repeater'

---
+++ Thinkful - String Drills: Repeater.js
@@ -0,0 +1,19 @@
+//https://www.codewars.com/kata/585a1a227cb58d8d740001c3/train/javascript
+
+//1.
+
+function repeater(string, n){
+    return string.repeat(n);
+}
+
+//2.
+
+// function repeater(string, n){
+//     let res = '';
+//     for (let i = 0; i < n; i++) {
+//         res += string;
+//     }
+//     return res;
+// }
+
+console.log(repeater('a', 5));
