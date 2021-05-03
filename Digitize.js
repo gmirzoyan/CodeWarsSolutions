c286649b1db6964f8556fba8b6f7c2ff0355b930
Gayane Mirzoyan <gayusha@gmail.com>
2021-05-02 23:41:57 -0700

    Add CodeWars Solution 'Digitize'

---
+++ Digitize.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/5417423f9e2e6c2f040002ae/train/javascript
+
+function digitize(n) {
+    return n.toString().split('').map(Number);
+}
+
+console.log(digitize(12345));
