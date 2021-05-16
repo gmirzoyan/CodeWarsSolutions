fa91e1bf45cec7a5eab57ad11fc8a980f392e6f6
Gayane Mirzoyan <gayusha@gmail.com>
2021-05-15 23:19:53 -0700

    Add CodeWars Solution 'Array Appender'

---
+++ Array Appender.js
@@ -0,0 +1,7 @@
+//https://www.codewars.com/kata/53a8a476947277a3020001cc/train/javascript
+
+function appendArrays (arr1, arr2) {
+    return arr1.concat(arr2);
+}
+
+console.log(appendArrays ([1,2],[3,4]));
