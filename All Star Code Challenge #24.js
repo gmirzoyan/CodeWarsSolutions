34af63407d0649aba09a8f61753c3a1f056fac31
Gayane Mirzoyan <gayusha@gmail.com>
2021-06-25 23:35:20 -0700

    Add CodeWars Solution 'All Star Code Challenge #24'

---
+++ All Star Code Challenge #24.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/5866c6cf442e3f16f9000089/train/javascript
+
+function hypotenuse(a, b){
+    return Math.sqrt((a ** 2) + (b ** 2));
+}
+
+function leg(c, a){
+    return Math.sqrt((c ** 2) - (a ** 2));
+}
