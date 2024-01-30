e0f6d8a62a2fd1fecf8465f40bcf08cdc269ad88
Gayane Mirzoyan <gayusha@gmail.com>
2024-01-30 02:18:14 -0800

    Add Second Solution

--- Duplicate Arguments.js
+++ Duplicate Arguments.js
@@ -1,7 +1,14 @@
 
+//1.
 function solution(){
-    let res = Object.values(arguments);
-    let r = res.filter((el, i) => res.indexOf(el) === i);
-    return res.length > r.length;
-  }
-  
+  let res = Object.values(arguments);
+  let r = res.filter((el, i) => res.indexOf(el) === i);
+  return res.length > r.length;
+}
+
+//2.
+function solution(){
+  let res = Object.values(arguments);
+  let r = [...new Set(res)]
+  return res.length > r.length;
+}
